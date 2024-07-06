a986566f0bf85659649aa9a316fe73853c5fa101
lxcaxguiaxi <luca.aguiari86@gmail.com>
2024-07-06 15:08:59 +0200

    ersten entwurf für Registry.jsx implementiert

--- vinoventure_mobile/Frontend/Login/Registry.jsx
+++ vinoventure_mobile/Frontend/Login/Registry.jsx
@@ -2,13 +2,34 @@ import React, { useState } from 'react';
 import {View, Text, Button, TextInput, Alert, StyleSheet} from "react-native";
 
 const Registry = ({navigation}) => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
     return(
         <View style={styles.container}>
-
+            <Text style={styles.title}>Registrierung</Text>
+            <TextInput
+                style={styles.input}
+                placeholder={'email@example.com'}
+                value={email}
+                placeholderTextColor={'#bbb'}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder={'Passwort'}
+                value={password}
+                placeholderTextColor={'#bbb'}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder={'Bestätigen Sie ihr Passwort'}
+                value={password}
+                placeholderTextColor={'#bbb'}
+                onChangeText={(text) => setPassword(text)}
+            />
         </View>
     );
 }
@@ -18,6 +39,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 16,
+    },
+    input: {
+        height: 40,
+        width: '80%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 16,
+        padding: 8,
     },
 });
 
