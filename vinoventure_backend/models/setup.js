af727ddcf77344685a0a1b5079a6eee5e42248c0
lxcaxguiaxi <luca.aguiari86@gmail.com>
2024-09-06 13:05:40 +0200

    setup.js mit erstem Tabellen model implementiert

---
+++ vinoventure_backend/models/setup.js
@@ -0,0 +1,13 @@
+const db = require("../config/database")
+
+const createTable = () => {
+    const createUserTable = `CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(60) NOT NULL,
+    lastname VARCHAR(60) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    birthdate Date NOT NULL,
+    created_at TIMESTAMP default CURRENT_TIMESTAMP
+    )`
+};
