f8959519b7c25791bda1d51e7f9441ada27c89be
lxcaxguiaxi <luca.aguiari86@gmail.com>
2024-09-06 10:05:53 +0200

    grundlegende struktur von server.js implementiert

--- vinoventure_backend/server.js
+++ vinoventure_backend/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`server läuft auf port ${port}`);
+})
