e235d695277d28874774dcbb8c75c4b1d0606d77
lxcaxguiaxi <luca.aguiari86@gmail.com>
2024-09-25 13:56:23 +0200

    signup routen in authRoutes und index.js hinzugefügt (bis jetzt 500 internal server error)

--- vinoventure_backend/routes/index.js
+++ vinoventure_backend/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const orderRoutes = require('./orderRoutes');
+const authRoutes = require('./authRoutes');
 
 // Verwende die Order-Routen
 router.use('/', orderRoutes);
+router.use('/users', authRoutes);
 
 module.exports = router;
