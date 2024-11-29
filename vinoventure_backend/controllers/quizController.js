03015848d816f8f3b79e261898e525b90d5564f6
Julian Holzer <julian.holzer@stud.it.htl-hl.ac.at>
2024-11-29 11:25:36 +0100

    QuizController hinzugefügt

---
+++ vinoventure_backend/controllers/quizController.js
@@ -0,0 +1,53 @@
+const { db } = require("../config/database");
+require("mysql2/promise");
+
+
+
+// Wine Quiz_id und Answers aus Wine_Packages rausziehen
+exports.createQuiz = async (req, res) => {
+    try {
+      const [rows] = await db.query(`SELECT * FROM orders`); // Verwende db.query() anstelle von db.all()
+      res.json({ users: rows });
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  };
+  
+
+
+exports.getAnswers
+   // Daten basierend auf der wineId
+  if (wineId === 3) {
+    const quizData = {
+      wineName: "Sauvignon Blanc | Scharl Vulkanland 2022",
+      wineId: 3,
+      quiz: {
+        smell: {
+          "MARACUJA": true,
+          "TOMATENRISPEN (STIELE)": false,
+          "PAPRIKA": false,
+          "JOHANNISBEERE": true
+        },
+        taste: {
+          "MARZIPAN": true,
+          "RAUCHIG": false,
+          "HASELNUSS": true,
+          "ROTE RÜBEN (ERDIG)": false
+        },
+        acidity: {
+          "WENIG": false,
+          "HOCH": true
+        },
+        finish: {
+          "VOLLMUNDIG - CREMIG": false,
+          "MINERALISCH - FRISCH": true
+        }
+      }
+    };
+
+    // Daten zurückgeben
+    res.json(quizData);
+  } else {
+    // Fehler für unbekannte wineId
+    res.status(404).json({ error: "Wein-Quiz nicht gefunden." });
+  };
---
+++ vinoventure_backend/routes/quizRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const quizController = require('../controllers/quizController')
+
+const router = express.Router();
+
+router.get('/get-Answers', quizController.getAnswers);
