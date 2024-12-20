b0a1e03ce457bdc96511b8c79a3d52eed680af9f
13nico01 <nico.z130601@gmail.com>
2024-12-20 09:10:04 +0100

    test

--- vinoventure_web/src/components/LoginRegisterComponents/RegisterComponent.jsx
+++ vinoventure_web/src/components/LoginRegisterComponents/RegisterComponent.jsx
@@ -23,7 +23,7 @@ const RegisterComponent = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center text-gray-800">Register hallo</h2>
+        <h2 className="text-2xl font-bold text-center text-gray-800">Register hallo0000000000</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">
