0bddee0221ea2910db607f06ae580f5173a4ced7
13nico01 <nico.z130601@gmail.com>
2024-12-26 22:32:22 +0100

    undo changes

--- vinoventure_backend/routes/index.js
+++ vinoventure_backend/routes/index.js
@@ -24,6 +24,5 @@ router.use("/order-manager", orderManagerRoutes);
 router.use("/cart", cartRoutes);
 router.use("/quiz", quizRoutes);
 router.use("/wine", wineRoutes);
-router.use('/images', express.static(path.join(__dirname, '../images')));
 
 module.exports = router;
