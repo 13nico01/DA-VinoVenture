cbd22c8041b938b68f76c63d6ceef253837b6afc
13nico01 <nico.z130601@gmail.com>
2024-12-27 12:35:17 +0100

    changed wine routes

--- vinoventure_backend/routes/wineRoutes.js
+++ vinoventure_backend/routes/wineRoutes.js
@@ -6,11 +6,11 @@ const wineController = require('../controllers/wineController');
 
 router.post('/update-image-paths', imageController.updateImagePaths);
 router.get('/get-wines-by-id/:packageId', imageController.getWinesByPackageId);
-router.get('/getImagesByPackageId/:packageId', imageController.getImagesByPackageId);
+router.get('/get-images-by-id/:packageId', imageController.getImagesByPackageId);
 router.post('/upload-image', imageController.uploadImage);
 router.get('/get-Wine', wineController.getWine)
-router.get('/getWineById/:id', wineController.getWineById);
-router.delete('/deleteWine/:id', wineController.deleteWine);
+router.get('/get-wine-by-id/:id', wineController.getWineById);
+router.delete('/delete-wine/:id', wineController.deleteWine);
 
 
 module.exports = router;
