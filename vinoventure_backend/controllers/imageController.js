ae97dd4bf56f87a8cde65e594cf7bfba6c5da0d9
13nico01 <nico.z130601@gmail.com>
2024-12-27 13:49:49 +0100

    debugging

--- vinoventure_backend/controllers/imageController.js
+++ vinoventure_backend/controllers/imageController.js
@@ -51,6 +51,8 @@ const upload = multer({ storage });
 
 exports.updateImagePaths = async (req, res) => {
     const imagesFolder = path.join(__dirname, '../images');
+    console.log('Image folder path:', imagesFolder);
+
 
     try {
         const [results] = await db.query('SELECT wine_id, image_name FROM wine');
