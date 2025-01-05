3648ff1d24f550c16a8673dc493ea6bbdf25425c
JULIXN <julian.holzer@it.htlhl.at>
2025-01-05 10:34:26 +0100

    changed cart Routes

--- vinoventure_backend/routes/cartRoutes.js
+++ vinoventure_backend/routes/cartRoutes.js
@@ -3,12 +3,12 @@ const cartController = require("../controllers/cartController");
 
 const router = express.Router();
 
-router.get("/getCart/:user_id", cartController.getCart);
+router.get("/get-Cart/:user_id", cartController.getCart);
 
-router.post("/addCart/:user_id", cartController.addToCart);
+router.post("/add-Cart/:user_id", cartController.addToCart);
 
-router.put("/updateCart/:user_id", cartController.updateCartItem);
+router.put("/update-Cart/:user_id", cartController.updateCartItem);
 
-router.delete("/deleteCart/:user_id/:wine_package_id", cartController.removeFromCart);
+router.delete("/delete-Cart/:user_id/:wine_package_id", cartController.removeFromCart);
 
 module.exports = router;
